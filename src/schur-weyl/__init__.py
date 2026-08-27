ed49a069dead5b0f471d0893c6b93b162816dc12
AVlyx <anton@vancleem.com>
2026-08-27 17:33:02 +0200

    set up and young diagrams formulas

---
+++ src/schur-weyl/__init__.py
@@ -0,0 +1,1 @@
+from young_diagrams import partitions, partition_conjugate, hook_lenght, cells, majorizes, addable_corners, removable_corners
---
+++ src/schur-weyl/dimensions.py
@@ -0,0 +1,9 @@
+def f_lambda(lam: list[int]) -> int:
+    """Return $f^\\lambda$, the dimension
+
+    Args:
+        lam (list[int]): _description_
+
+    Returns:
+        int: _description_
+    """
